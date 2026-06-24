920bff5bebc948527f0060a1e9a522617013f862
mukesh.pyla <mukesh.pyla@coditas.com>
2026-06-24 11:01:10 +0530

    Started with the basic set-up of project. Built a functional-requirements documentation for the alloted problem statement.

---
+++ src/test/java/com/mukesh/internCapstoneProject/InternCapstoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mukesh.internCapstoneProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternCapstoneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
